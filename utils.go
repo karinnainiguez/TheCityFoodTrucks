40d4288fcf3a6f16f1ab1bab0c1bf0082c2579bf
Karinna Iniguez <kngz@amazon.com>
2018-11-16 17:24:25 -0800

    Implement paginate function

--- utils.go
+++ utils.go
@@ -13,3 +13,17 @@ func handle(err error) {
 	}
 
 }
+
+func paginate(trucks []truck, skip int, size int) []truck {
+	if skip > len(trucks) {
+		skip = len(trucks)
+	}
+
+	end := skip + size
+	if end > len(trucks) {
+		end = len(trucks)
+	}
+
+	currentCollection := trucks[skip:end]
+	return currentCollection
+}
